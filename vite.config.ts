5aeebbca9a53592e7229168ad66849113638d070
einaralex <einaralex@gmail.com>
2023-01-04 09:18:32 +0000

    chore: cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import path from "path";
 import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "./",
-  //   plugins: [react()],
   build: {
     sourcemap: true,
     lib: {
@@ -13,14 +11,5 @@ export default defineConfig({
       //   formats: ["es", "cjs", "umd", "iife"],
       fileName: (format) => `index.${format}.js`,
     },
-    // rollupOptions: {
-    //   external: ["react", "react-dom"],
-    //   output: {
-    //     globals: {
-    //       react: "React",
-    //       "react-dom": "ReactDOM",
-    //     },
-    //   },
-    // },
   },
 });
