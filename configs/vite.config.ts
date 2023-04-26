794c2b0d804eb1ae5c4c7a8d447e617408adfbaa
einaralex <einaralex@gmail.com>
2023-04-26 10:51:34 +0000

    chore: move vite config

--- configs/vite.config.ts
+++ configs/vite.config.ts
@@ -2,11 +2,10 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
   build: {
     sourcemap: true,
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: path.resolve(__dirname, '../src/index.ts'),
       name: 'monerium-sdk',
       //   formats: ["es", "cjs", "umd", "iife"],
       fileName: (format) => {
