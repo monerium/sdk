8a15ab396000d60b2b13ad2bbbe4e334532f9f36
einaralex <einaralex@gmail.com>
2023-04-28 14:34:46 +0000

    Deployed SDK Docs & Code Coverage to Github Pages (https://monerium.github.io/sdk)

--- configs/vite.config.ts
+++ configs/vite.config.ts
@@ -8,13 +8,7 @@ export default defineConfig({
       entry: path.resolve(__dirname, '../src/index.ts'),
       name: 'monerium-sdk',
       //   formats: ["es", "cjs", "umd", "iife"],
-      fileName: (format) => {
-        if (format === 'es') {
-          return 'index.mjs';
-        } else {
-          return `index.${format}.js`;
-        }
-      },
+      fileName: 'index',
     },
   },
 });
