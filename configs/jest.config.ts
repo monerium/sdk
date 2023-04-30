5d29a6ded6d4f038444f75da0d115039003de9c9
Einar Alexander Eymundsson <einaralex@gmail.com>
2023-04-30 14:47:39 +0000

    Smoke test for bundles (#83)

--- configs/jest.config.ts
+++ configs/jest.config.ts
@@ -4,8 +4,8 @@ const config: Config = {
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)s$',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j|mj)s$',
+  moduleFileExtensions: ['ts', 'js', 'mjs', 'json', 'node'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['../.'],
