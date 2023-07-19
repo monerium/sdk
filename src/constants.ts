600a6348f4f6ce0b3f2cc472c807520620bc0d60
einaralex <einaralex@gmail.com>
2023-07-19 11:54:32 +0000

    feat: removed Chain and Network enum
    fix: constants typed import

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,6 @@
 export const LINK_MESSAGE = `I hereby declare that I am the address owner.`;
 
-export enum Network {
-  Goerli = 'goerli',
-  Chiado = 'chiado',
-  Mumbai = 'mumbai',
-  Mainnet = 'mainnet',
-}
-
-export enum Chain {
-  Ethereum = 'ethereum',
-  Gnosis = 'gnosis',
-  Polygon = 'polygon',
-}
+const constants = {
+  LINK_MESSAGE,
+};
+export default constants;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { MoneriumClient } from './client';
+export { default as constants } from './constants';
 export * from './types';
-export * as constants from './constants';
 export { placeOrderMessage, rfc3339 } from './utils';
