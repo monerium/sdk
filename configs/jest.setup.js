a8888b5ed8ee610703ca46b6986113a0a11081eb
einaralex <einaralex@gmail.com>
2023-11-13 14:22:54 +0000

    remove code and add link to the new repo

--- configs/jest.setup.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const fetchMock = require('jest-fetch-mock');
-global.fetch = fetchMock;
-fetchMock.dontMock();
--- src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const LINK_MESSAGE = `I hereby declare that I am the address owner.`;
-
-const constants = {
-  LINK_MESSAGE,
-};
-export default constants;
